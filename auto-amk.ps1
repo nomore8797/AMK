aa55c4796d93a1d75ec431326b78cead2c6d567c
nomore8797 <nomore8797@gmail.com>
2025-12-19 14:23:28 +0300

    Update auto-amk.ps1

--- auto-amk.ps1
+++ auto-amk.ps1
@@ -5,27 +5,35 @@ $localFile = "C:\AMK\index.html"
 $containerPath = "/usr/share/nginx/html/index.html"
 $port = 8080
 
+# Проверяем, что файл существует
+if (-Not (Test-Path $localFile)) {
+    Write-Host "Файл $localFile не найден!"
+    exit
+}
+
 # Останавливаем и удаляем старый контейнер (если есть)
 if (docker ps -a --format "{{.Names}}" | Select-String $containerName) {
     Write-Host "Останавливаем и удаляем старый контейнер..."
     docker rm -f $containerName
 }
 
-# Запускаем новый контейнер
-Write-Host "Запускаем новый контейнер..."
+# Запускаем новый контейнер с пробросом порта и монтированием файла
+Write-Host "Запускаем контейнер $containerName на порту $port..."
 docker run -d -p $port:80 --name $containerName -v "$localFile":"$containerPath" $imageName
 
 Write-Host "Контейнер запущен. Проверь в браузере: http://localhost:$port"
 
-# Запускаем мониторинг файла
+# Запускаем авто-обновление
 $lastWrite = (Get-Item $localFile).LastWriteTime
+Write-Host "Запущен мониторинг файла: $localFile"
+Write-Host "Для выхода нажмите Ctrl+C"
 
 while ($true) {
     Start-Sleep -Seconds 1
     $currentWrite = (Get-Item $localFile).LastWriteTime
     if ($currentWrite -ne $lastWrite) {
-        # Файл изменился
-        docker cp $localFile $containerName:$containerPath
+        $dest = "$containerName:$containerPath"
+        docker cp $localFile $dest
         docker exec $containerName nginx -s reload
         Write-Host "[$(Get-Date -Format 'HH:mm:ss')] index.html обновлён в контейнере."
         $lastWrite = $currentWrite
