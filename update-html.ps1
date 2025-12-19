a6822e0d4e443e6497328d1b0705ee2683de3bc1
nomore8797 <nomore8797@gmail.com>
2025-12-19 14:20:27 +0300

    Update update-html.ps1

--- update-html.ps1
+++ update-html.ps1
@@ -3,30 +3,27 @@ $localFile = "C:\AMK\index.html"
 $containerName = "amk-test"
 $containerPath = "/usr/share/nginx/html/index.html"
 
-# Проверка наличия файла
-if (-not (Test-Path $localFile)) {
+# Проверяем, что файл существует
+if (-Not (Test-Path $localFile)) {
     Write-Host "Файл $localFile не найден!"
     exit
 }
 
-# Сохраняем время последнего изменения
+# Получаем время последнего изменения файла
 $lastWrite = (Get-Item $localFile).LastWriteTime
 
-Write-Host "Начинаем слежение за $localFile..."
+Write-Host "Запущен мониторинг файла: $localFile"
+Write-Host "Для выхода нажмите Ctrl+C"
+
 while ($true) {
     Start-Sleep -Seconds 1
     $currentWrite = (Get-Item $localFile).LastWriteTime
     if ($currentWrite -ne $lastWrite) {
         # Файл изменился
-        Write-Host "[$(Get-Date -Format 'HH:mm:ss')] Файл изменился. Обновляем контейнер..."
-        
-        # Копируем файл в контейнер
-        docker cp $localFile "$containerName:$containerPath"
-        
-        # Перезапускаем nginx внутри контейнера
-        docker exec $containerName sh -c "nginx -s reload"
-        
-        Write-Host "[$(Get-Date -Format 'HH:mm:ss')] Контейнер обновлён!"
+        $dest = "$containerName:$containerPath"
+        docker cp $localFile $dest
+        docker exec $containerName nginx -s reload
+        Write-Host "[$(Get-Date -Format 'HH:mm:ss')] index.html обновлён в контейнере."
         $lastWrite = $currentWrite
     }
 }
