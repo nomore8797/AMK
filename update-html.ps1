3928795720647343aa33142e0a5f2794f4b40a3c
nomore8797 <nomore8797@gmail.com>
2025-12-19 10:01:22 +0300

    Update update-html.ps1

--- update-html.ps1
+++ update-html.ps1
@@ -1,24 +1,25 @@
-# Пути и контейнер
 $localFile = "C:\AMK\index.html"
 $containerName = "amk-test"
 $containerPath = "/usr/share/nginx/html/index.html"
 
-# Следим за временем изменения файла
+# Проверяем, что файл существует
+if (-not (Test-Path $localFile)) {
+    Write-Host "Файл $localFile не найден."
+    exit
+}
+
 $lastWrite = (Get-Item $localFile).LastWriteTime
+
+Write-Host "Запуск мониторинга $localFile ..."
 
 while ($true) {
     Start-Sleep -Seconds 1
     $currentWrite = (Get-Item $localFile).LastWriteTime
     if ($currentWrite -ne $lastWrite) {
-        try {
-            # Копируем файл в контейнер
-            & docker cp $localFile "${containerName}:$containerPath"
-            # Перезагружаем nginx
-            & docker exec $containerName nginx -s reload
-            Write-Host "[$(Get-Date -Format 'HH:mm:ss')] index.html обновлён в контейнере."
-        } catch {
-            Write-Host "Ошибка при обновлении файла: $_"
-        }
+        # Файл изменился
+        docker cp $localFile $containerName:`$containerPath
+        docker exec $containerName nginx -s reload
+        Write-Host "[$(Get-Date -Format 'HH:mm:ss')] index.html обновлён в контейнере."
         $lastWrite = $currentWrite
     }
 }
