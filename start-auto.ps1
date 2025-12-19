2cbff606b37fc68784cae7cf3ba5a68cb53e9722
nomore8797 <nomore8797@gmail.com>
2025-12-19 12:41:36 +0300

    Update start-auto.ps1

--- start-auto.ps1
+++ start-auto.ps1
@@ -5,33 +5,31 @@ $localFile = "C:\AMK\index.html"
 $containerPath = "/usr/share/nginx/html/index.html"
 $port = 8080
 
-# Проверяем, существует ли контейнер
-$containerExists = docker ps -a --format "{{.Names}}" | Select-String $containerName
-
-if ($containerExists) {
+# Удаляем старый контейнер, если существует
+if (docker ps -a --format "{{.Names}}" | Select-String $containerName) {
     Write-Host "Останавливаем и удаляем старый контейнер..."
     docker rm -f $containerName
 }
 
-# Запускаем новый контейнер с пробросом порта и монтированием файла
+# Запускаем новый контейнер
 Write-Host "Запускаем новый контейнер..."
 docker run -d -p $port:80 --name $containerName -v "$localFile":"$containerPath" $imageName
-
 Write-Host "Контейнер запущен. Проверяй в браузере: http://localhost:$port"
 
-# --- Автообновление ---
-# Получаем время последнего изменения файла
-$lastWrite = (Get-Item $localFile).LastWriteTime
-
-Write-Host "Запущен мониторинг файла $localFile. Нажмите Ctrl+C для остановки."
-
-while ($true) {
+# Автообновление index.html в отдельном PowerShell окне
+$updateScript = @"
+\$lastWrite = (Get-Item '$localFile').LastWriteTime
+while (\$true) {
     Start-Sleep -Seconds 1
-    $currentWrite = (Get-Item $localFile).LastWriteTime
-    if ($currentWrite -ne $lastWrite) {
-        docker cp $localFile "${containerName}:$containerPath"
+    \$currentWrite = (Get-Item '$localFile').LastWriteTime
+    if (\$currentWrite -ne \$lastWrite) {
+        docker cp '$localFile' '${containerName}:$containerPath'
         docker exec $containerName nginx -s reload
-        Write-Host "[$(Get-Date -Format 'HH:mm:ss')] index.html обновлён в контейнере."
-        $lastWrite = $currentWrite
+        Write-Host "[\$(Get-Date -Format 'HH:mm:ss')] index.html обновлён в контейнере."
+        \$lastWrite = \$currentWrite
     }
 }
+"@
+
+Start-Process powershell -ArgumentList "-NoExit", "-Command $updateScript"
+Write-Host "Автообновление запущено в отдельном окне PowerShell."
