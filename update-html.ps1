7a281e1ac3d972808e0918daa773d6e3d6ad88e2
nomore8797 <nomore8797@gmail.com>
2025-12-19 08:40:05 +0300

    Update update-html.ps1

--- update-html.ps1
+++ update-html.ps1
@@ -1,18 +1,27 @@
+# Путь к локальному файлу
 $localFile = "C:\AMK\index.html"
+
+# Имя контейнера
 $containerName = "amk-test"
+
+# Путь внутри контейнера
 $containerPath = "/usr/share/nginx/html/index.html"
 
-# Запускаем мониторинг файла
-$lastWrite = (Get-Item $localFile).LastWriteTime
+# Предыдущее время изменения файла
+$lastWriteTime = (Get-Item $localFile).LastWriteTime
+
+Write-Host "Автообновление index.html каждые 10 секунд. Ctrl+C для выхода."
 
 while ($true) {
-    Start-Sleep -Seconds 1
-    $currentWrite = (Get-Item $localFile).LastWriteTime
-    if ($currentWrite -ne $lastWrite) {
-        # Файл изменился
-        docker cp $localFile $containerName:$containerPath
+    Start-Sleep -Seconds 10
+
+    $currentWriteTime = (Get-Item $localFile).LastWriteTime
+
+    if ($currentWriteTime -gt $lastWriteTime) {
+        # Файл обновлён
+        docker cp "$localFile" "${containerName}:$containerPath"
         docker exec $containerName nginx -s reload
-        Write-Host "[$(Get-Date -Format 'HH:mm:ss')] index.html обновлён в контейнере."
-        $lastWrite = $currentWrite
+        Write-Host "$(Get-Date -Format 'HH:mm:ss') - Файл обновлён и Nginx перезагружен."
+        $lastWriteTime = $currentWriteTime
     }
 }
